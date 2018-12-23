26af203c31bd2dcf44ac86162aaff2a18a060044
Pmc Dev <phoomparin@gmail.com>
2018-12-24 00:38:36 +0700

    :memo: Remove server.js

--- illudesktop/main.js
+++ illudesktop/main.js
@@ -1,19 +1,15 @@
-// Modules to control application life and create native browser window
 const electron = require('electron')
 const {PeerServer} = require('peer')
 
-const createServer = require('./server')
-
 const {app, BrowserWindow} = electron
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const RTC_PORT = 9000
+
 let mainWindow
 
 function createWindow() {
   const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
 
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width,
     height,
@@ -24,30 +20,17 @@ function createWindow() {
   mainWindow.setIgnoreMouseEvents(true)
   mainWindow.setAlwaysOnTop(true)
 
-  // and load the index.html of the app.
-  // mainWindow.loadFile('index.html')
   mainWindow.loadURL('http://localhost:3000')
 
-  // Open the DevTools.
   mainWindow.webContents.openDevTools({mode: 'detach'})
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
 
-const server = createServer()
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
@@ -59,10 +42,13 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (!mainWindow) {
     createWindow()
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Enable WebRTC server
+PeerServer({
+  debug: true,
+  port: RTC_PORT
+})
